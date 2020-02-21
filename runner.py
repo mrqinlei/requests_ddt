402e8561cb359d1335bd8fb47b787efa21b7e33b
qinlei <mrqinlei@gmail.com>
2020-02-21 19:33:03 +0800

    add ddt function

--- runner.py
+++ runner.py
@@ -34,16 +34,13 @@ desc = '老黄历接口测试'
 
 if not os.path.exists(report_path):
     os.makedirs(report_path)
-test_data = Do_Excel("test_data.xlsx", "oldyellowhistory").get_data()
-suite = unittest.TestSuite()
-for item in test_data:
-    suite.addTest(TestHttp("api_test",item['url'],eval(item['data']),item['except1'],item['except2'],item['method']))
-print(type(item),item)
-runner = BeautifulReport(suite)
-runner.report(description=desc,filename=report_title,report_dir=report_path)
-# if __name__ == '__main__':
-    # test_data = Do_Excel("test_data.xlsx", "oldyellowhistory").get_data()
-    # suite = unittest.TestSuite()
-    # suite.addTest(TestHttp("test_api",test_data[0]['url'],test_data[0]['data'],test_data[0]['except1'],test_data[0]['except2'],test_data[0]['method']))
-    # runner = BeautifulReport(suite)
-    # runner.report(description=desc,filename=report_title,report_dir=report_path)
+
+
+#ddt 不能通过创建实例来存储用例，可以用loader
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    loader = unittest.TestLoader()
+    suite.addTest(loader.loadTestsFromTestCase(TestHttp))
+    runner = BeautifulReport(suite)
+    runner.report(description=desc, filename=report_title, report_dir=report_path)
--- testsuite/test_http.py
+++ testsuite/test_http.py
@@ -4,33 +4,34 @@
 # @File     :lottery_test_case.py
 import unittest
 from tools.http_request import HttpRequest
+from ddt import ddt, data
+from tools.do_excel import Do_Excel
 
-
+test_data = Do_Excel("test_data.xlsx", "oldyellowhistory").get_data()
+@ddt
 class TestHttp(unittest.TestCase):
     def setUp(self) -> None:
 
         print("开始执行")
 
-    def __init__(self,methodName,url,data,except1,except2,method):
-        super(TestHttp,self).__init__(methodName)
-        self.url = url
-        self.data = data
-        self.method = method
-        self.except1 = except1
-        self.except2 = except2
+    # def __init__(self,methodName,url,data,except1,except2,method):
+    #     super(TestHttp,self).__init__(methodName)
+    #     self.url = url
+    #     self.data = data
+    #     self.method = method
+    #     self.except1 = except1
+    #     self.except2 = except2
 
-
-
-    def api_test(self):
-        res = HttpRequest().http_request(self.url, self.data,self.method)
+    @data(*test_data)
+    def test_api(self,item):
+        res = HttpRequest().http_request(item['url'], eval(item['data']), item['http_method'])
         try:
-            self.assertEqual(self.except1, res['error_code'])
-            self.assertEqual(self.except2, res['reason'])
+            self.assertEqual(item['except1'], res['error_code'])
+            self.assertEqual(item['except2'], res['reason'])
+            print("相应内容{0}\n请求结果{1}\n请求错误码为{2}".format(res['result'],res['reason'],res['error_code']))
         except AssertionError as e:
             print("出错了{0}".format(e))
             raise e
-
-
 
     def tearDown(self) -> None:
         print("结束执行")
